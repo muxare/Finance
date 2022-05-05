321cd2855dd5cf63695c3895d13011ecf45c55a6
Mikael Axelsson <mikael.axelsson@teliacompany.com>
2022-05-05 20:18:12 +0200

    Add controller for quote import

--- Finance.Api/Controllers/CompaniesController.cs
+++ Finance.Api/Controllers/CompaniesController.cs
@@ -6,29 +6,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Finance.Api.Controllers
 {
+
     [ApiController]
     [Route("[controller]")]
     public class CompaniesController : ControllerBase
     {
         private string CompanyPartitionKey = "d308f72a-cc29-47d0-812e-178351c30bc4";
+        private string connectionString = "DefaultEndpointsProtocol=https;AccountName=financequotestorage;AccountKey=zF9IfHEw6C1FVGcoC07HngYDvvgsBuHc+Ww1U2fqTIdAIZUyqUIJs3ef4ZR8+Z5/kzHi95igdy1Pm+uuIOp12w==;EndpointSuffix=core.windows.net";
+        private string tablename = "Companies";
+
         public CompaniesController()
         {
-
         }
 
         [HttpGet(Name = "GetCompanies")]
         public async Task<IEnumerable<CompanyEntity>> GetCompaniesAsync()
         {
-            var connectionString = "DefaultEndpointsProtocol=https;AccountName=financequotestorage;AccountKey=zF9IfHEw6C1FVGcoC07HngYDvvgsBuHc+Ww1U2fqTIdAIZUyqUIJs3ef4ZR8+Z5/kzHi95igdy1Pm+uuIOp12w==;EndpointSuffix=core.windows.net";
-            var tablename = "Companies";
             //var tableServiceClient = new TableServiceClient(connectionString);
             var tableClient = new TableClient(connectionString, tablename);
             await tableClient.CreateIfNotExistsAsync();
             Pageable<CompanyEntity> result = tableClient.Query<CompanyEntity>();
-
-            //var rest = await tableServiceClient.CreateTableIfNotExistsAsync(tablename);
-
-            
 
             return result;
         }
@@ -36,9 +33,6 @@ namespace Finance.Api.Controllers
         [HttpPost(Name = "AddCompany")]
         public async Task AddCompany(string name, string ticker, string url)
         {
-            var connectionString = "DefaultEndpointsProtocol=https;AccountName=financequotestorage;AccountKey=zF9IfHEw6C1FVGcoC07HngYDvvgsBuHc+Ww1U2fqTIdAIZUyqUIJs3ef4ZR8+Z5/kzHi95igdy1Pm+uuIOp12w==;EndpointSuffix=core.windows.net";
-            var tablename = "Companies";
-
             var companyEntity = new CompanyEntity
             {
                 Name = name,
---
+++ Finance.Api/Controllers/QuotesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuotesController : ControllerBase
+    {
+        // Use client for data lake to store quote data
+
+        [HttpPost(Name = "StartImport")]
+        public Task GetCompaniesAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
